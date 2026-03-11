db706bf7df63839d1c2fe8b0934fc24525827c14
Emanuel033 <52466996+Emanuel033@users.noreply.github.com>
2026-03-10 20:06:12 -0600

    Update App.jsx
    Modo admin

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,32 @@ import ProductGrid from './components/ProductGrid'
 import Footer from './components/Footer'
 import FloatingButtons from './components/FloatingButtons'
 import CartDrawer from './components/CartDrawer'
-import QRScanner from './components/QRScanner' // <--- IMPORTAMOS
-import BackupTool from './components/BackupTool' // <--- IMPORTAMOS
+import QRScanner from './components/QRScanner' 
+import BackupTool from './components/BackupTool' 
+import { useApp } from './context/AppContext' // <--- IMPORTAMOS EL CONTEXTO
 
 function App() {
+  // Extraemos el estado de admin
+  const { esAdmin, setEsAdmin } = useApp();
+
   return (
     <div className="bg-slate-50 font-sans text-slate-800 flex flex-col min-h-screen relative overflow-x-hidden">
       
       <Navbar />
       <Hero />
-    
       <CategoriesBar />
        
       <main className="flex-grow max-w-7xl mx-auto px-4 py-8 w-full">
+        {/* Renderizamos la herramienta si es Admin */}
+        {esAdmin && <BackupTool onClose={() => setEsAdmin(false)} />}
+        
         <ProductGrid />
       </main>
 
       <Footer />
       <CartDrawer />
       <FloatingButtons />
-      
-      <QRScanner /> {/* <--- LO AGREGAMOS AQUÍ */}
+      <QRScanner />
 
     </div>
   )
