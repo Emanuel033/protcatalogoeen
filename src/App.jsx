038f670ebfe9a46a3e2ff5ac02208f835943b38b
Emanuel033 <52466996+Emanuel033@users.noreply.github.com>
2026-05-06 11:33:53 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import { LogisticaProvider } from './pages/Rutas/context/LogisticaContext';
 import AdminLayout from './pages/Admin/AdminLayout'; 
 
 // Corrección: Importamos ChoferView con su nombre correcto
-import ChoferView from './pages/Chofer/ChoferView';
+import ChoferView from './pages/Chofer/AppOperador';
 
 function App() {
   return (
