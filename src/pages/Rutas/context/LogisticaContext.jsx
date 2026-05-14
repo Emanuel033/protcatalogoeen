c6cf38988ffd108fb64ddd8007670499c30c76eb
Emanuel033 <52466996+Emanuel033@users.noreply.github.com>
2026-05-14 15:12:27 -0600

    Update LogisticaContext.jsx

--- src/pages/Rutas/context/LogisticaContext.jsx
+++ src/pages/Rutas/context/LogisticaContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { collection, onSnapshot, doc, writeBatch, arrayUnion, getDocs } from 'firebase/firestore';
-import { db } from '../../../firebase'; 
+import { db } from '../../../firebase.js';
 
 const LogisticaContext = createContext();
 
